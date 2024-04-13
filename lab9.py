beb38f68571db0cbf5d076950e053c6154b1fde1
Mandar Ghade <GX2H912@gmail.com>
2024-04-13 17:20:23 -0400

    Added decode function.

--- lab9.py
+++ lab9.py
@@ -7,17 +7,21 @@ def main():
         print('1. Encode')
         print('2. Decode')
         print('3. Quit')
+        print()
         menu_option = int(input('Please enter an option: '))
         if menu_option == 1:
             password = input('Please enter your password to encode: ')
             current_password = encode(password)
             print('Your password has been encoded and stored!')
         elif menu_option == 2:
-            decode(current_password)
+            decoded_password = decode(current_password)
+            print(f'The encoded password is {current_password}, and the original password is {decoded_password}.')
         elif menu_option == 3:
             break
         else:
             continue
+
+
 def encode(password):
     encrypted_password = ''
     for num in password:
@@ -26,7 +30,9 @@ def encode(password):
 
 
 def decode(encrypted_password):
-    pass
+    return ''.join(str(10+int(c)-3)[-1]
+                   for c in encrypted_password)
+
 
 if __name__ == '__main__':
    main()
